ea9a2cd534390819a2e596708a73cf04b7fa3b2d
ajitkumar <ajitkumar.sn8@gmail.com>
2025-04-11 10:58:00 +0530

    Multiple route handlers

--- src/app.js
+++ src/app.js
@@ -5,9 +5,27 @@ const express = require("express");
 const app = express();
 const port = 2000;
 
-app.get("/user/:userId/:userName", (req, res) => {
-    console.log(req.params);
-    res.send({firstName: "Ajit", lastName: "Kumar"});
+//app.use("/route", rH1, [rH2, rH3], rH4, rH5); - you can group with it array also and it worked the same
+
+app.get("/user", (req, res, next) => {
+    console.log("Handling the route user 1!");
+    //res.send("1st Response");
+    next();
+}, (req, res, next) => {
+    console.log("Handling the route user 2!");
+    //res.send("2nd Response");
+    next();
+}, (req, res, next) => {
+    console.log("Handling the route user 3!");
+    //res.send("3rd Response");
+    next();
+}, (req, res, next) => {
+    console.log("Handling the route user 4!");
+    //res.send("4th Response");
+    next();
+}, (req, res, next) => {
+    console.log("Handling the route user 5!");
+    res.send("5th Response");
 });
 
 app.listen(port, () => {
