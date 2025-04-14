d8c517e8a335bc6e3e9f640394b6c20293316e1a
ajitkumar <ajitkumar.sn8@gmail.com>
2025-04-14 23:48:21 +0530

    Exploring data validators

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -16,10 +17,20 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address: "+ value);
+            }
+        },
     },
     password: {
         type: String,
         required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Please enter a strong password: "+ value);
+            }
+        },
     },
     age: {
         type: Number,
@@ -36,6 +47,11 @@ const userSchema = new mongoose.Schema({
     photoUrl: {
         type: String,
         default: "https://image.pngaaa.com/853/3873853-middle.png",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid photo URL: "+ value);
+            }
+        },
     },
     about: {
         type: String,
