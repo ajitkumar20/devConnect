e18772fe48121a61aa62659e5e423347392829a7
ajitkumar <ajitkumar.sn8@gmail.com>
2025-04-14 23:08:10 +0530

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -71,11 +71,23 @@ app.delete("/user", async (req, res) => {
 });
 
 // Update a user by ID
-app.patch("/user", async (req, res) => {
-    const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+    const userId = req.params?.userId;
     const data = req.body;
 
     try {
+        const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+
+       const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+
+       if(!isUpdateAllowed){
+        throw new Error("Update not allowed!");
+       }
+
+       if(data?.skills.length > 15){
+        throw new Error("Skills cannot be more than 15");
+       }
+
         const user = await User.findByIdAndUpdate({_id: userId}, data, {returnDocument: "after", runValidators: true});
         //console.log(user) - return the document after/before changes made
         res.send("User updated successfully!")
