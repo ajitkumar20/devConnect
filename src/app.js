7542876d8292d659fa8069d1f2a0e1057adb5e53
ajitkumar <ajitkumar.sn8@gmail.com>
2025-04-14 18:31:20 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -76,10 +76,11 @@ app.patch("/user", async (req, res) => {
     const data = req.body;
 
     try {
-        const user = await User.findByIdAndUpdate({_id: userId}, data);
+        const user = await User.findByIdAndUpdate({_id: userId}, data, {returnDocument: "after", runValidators: true});
+        //console.log(user) - return the document after/before changes made
         res.send("User updated successfully!")
     } catch (err) {
-        res.status(400).send("Something went wrong!");
+        res.status(400).send("Something went wrong: "+ err.message);
     }
 });
 
