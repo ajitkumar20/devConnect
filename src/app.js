80e411f2d9e7f2d97623ebe62d6158fcc0276017
ajitkumar <ajitkumar.sn8@gmail.com>
2025-04-15 11:38:41 +0530

    Validation of signup data and password encryption

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
+const {validateSignUpData} = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const port = 2000;
@@ -9,24 +11,22 @@ app.use(express.json());
 
 // Create a new user
 app.post("/signup", async (req, res) => {
-
-    //console.log(req.body);
-
-    // const userObj = {
-    //     firstName: "Aniket",
-    //     lastName: "Kumar",
-    //     emailId: "aniketkumar@gmail.com",
-    //     password: "Aniket@123",
-    // };
-
-    // Creating a new instance of the User model
-    const user = new User(req.body);
-
     try {
+        // Validation of data
+        validateSignUpData(req);
+
+        const {firstName, lastName, emailId, password} = req.body;
+        // Encrypt the password
+        const passwordHash = await bcrypt.hash(password, 10);
+        console.log(passwordHash);
+
+        // Creating a new instance of the User model
+        const user = new User({firstName, lastName, emailId, password: passwordHash});
+
         await user.save();
         res.send("User added successfully!");
     } catch(err) {
-        res.status(400).send("Error saving the user: " + err.message);
+        res.status(400).send("ERROR: " + err.message);
     }
 });
 
